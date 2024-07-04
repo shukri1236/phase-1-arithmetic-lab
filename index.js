ebadac3aae84c4d508970e3fbcea60460a4c9f47
shukri1236 <shukri1236>
2024-07-04 21:47:53 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+// Write your code here
+// Write your code here
+let num1 = 1
+let num2 = 62
+
+// Write your code here
+let multiply = num1*num2 
+
+let random = 10
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+let max = 20
